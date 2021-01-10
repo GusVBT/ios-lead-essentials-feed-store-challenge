bd926b8b9937b44ae3713f701ac367460d3d19c7
Gustavo Arthur Vollbrecht <gus.vbt@gmail.com>
2021-01-10 16:46:36 +0000

    CoreDataFeedStore retrieve delivers empty on empty cache

--- Tests/FeedStoreIntegrationTests.swift
+++ Tests/FeedStoreIntegrationTests.swift
@@ -27,9 +27,9 @@ class FeedStoreIntegrationTests: XCTestCase {
 	}
 	
 	func test_retrieve_deliversEmptyOnEmptyCache() {
-		//        let sut = makeSUT()
-		//
-		//        expect(sut, toRetrieve: .empty)
+		let sut = makeSUT()
+
+		expect(sut, toRetrieve: .empty)
 	}
 	
 	func test_retrieve_deliversFeedInsertedOnAnotherInstance() {
@@ -72,15 +72,35 @@ class FeedStoreIntegrationTests: XCTestCase {
 	// - MARK: Helpers
 	
 	private func makeSUT() -> FeedStore {
-		fatalError("Must be implemented")
+		let storeURL = self.storeURL()
+		let bundle = Bundle(for: CoreDataFeedStore.self)
+		
+		let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: bundle)
+		trackForMemoryLeaks(sut)
+		return sut
+	}
+	
+	private func storeURL() -> URL {
+		let cachesDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+		let storeURL = cachesDirectory.appendingPathComponent("\(type(of: self)).store")
+		return storeURL
 	}
 	
 	private func setupEmptyStoreState() {
-		
+		self.deleteArtifacts()
 	}
 	
 	private func undoStoreSideEffects() {
-		
+		self.deleteArtifacts()
 	}
 	
+	private func deleteArtifacts() {
+		if FileManager.default.fileExists(atPath: self.storeURL().path) {
+			do {
+				try FileManager.default.removeItem(at: self.storeURL())
+			} catch {
+				XCTFail("Failed to remove items at \(self.storeURL().absoluteString)")
+			}
+		}
+	}
 }
